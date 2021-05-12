f9b17946191cd290918cd8c9945b73d7ccb3bf6d
sergiu <frunza.sergiu3@gmai.com>
2021-05-12 14:53:56 -0400

    added projects

--- client/src/views/pages/Projects/data.js
+++ client/src/views/pages/Projects/data.js
@@ -1,25 +1,49 @@
-import moving from 'src/assets/img/insight.png'
-import weather from 'src/assets/img/weather.png'
+import moving from "src/assets/img/insight.png";
+import weather from "src/assets/img/weather.png";
+import tailor from "src/assets/img/tailor.png";
+import time from "src/assets/img/time.png";
 
 export const news = [
   {
     cover: {
-      src: `${moving}`,
-      srcSet: `${moving}`,
+      src: `${time}`,
+      srcSet: `${time}`
     },
-    title: 'InsightMoving',
-    description: 'A Moving Company website with full Customer Relationship Management.',
-    href: 'https://www.insightmoving.com/',
-    tech: 'React.js, Rails5, PostgreSQL, Google Maps API, AWS S3'
+    title: "Time Calculator",
+    description: "Labour time calculator and the total amount.",
+    href: "https://timecalc-app-b0fc0.web.app",
+    tech: "React.js"
+  },
+  {
+    cover: {
+      src: `${tailor}`,
+      srcSet: `${tailor}`
+    },
+    title: "Tailor Made",
+    description: "Website for a local Tailor Shop.",
+    href: "https://tailormadeboston.com/",
+    tech: "React.js, Material UI, Firebase"
+  },
+  {
+    cover: {
+      src: `${moving}`,
+      srcSet: `${moving}`
+    },
+    title: "InsightMoving",
+    description:
+      "A Moving Company website with full Customer Relationship Management.",
+    href: "https://www.insightmoving.com/",
+    tech: "React.js, Rails5, PostgreSQL, Google Maps API, AWS S3"
   },
   {
     cover: {
       src: `${weather}`,
-      srcSet: `${weather}`,
+      srcSet: `${weather}`
     },
-    title: 'Weather Review',
-    description: 'The Weather Review App provides users a place to add reviews to cities based on the particular city\'s weather.',
-    href: 'https://weather-review.herokuapp.com/',
-    tech: 'RoR, React.js, JQuery, PostgreSQL, Devise'
-  },
+    title: "Weather Review",
+    description:
+      "The Weather Review App provides users a place to add reviews to cities based on the particular city's weather.",
+    href: "https://weather-review.herokuapp.com/",
+    tech: "RoR, React.js, JQuery, PostgreSQL, Devise"
+  }
 ];
--- client/src/views/pages/Resume/data.js
+++ client/src/views/pages/Resume/data.js
@@ -1,95 +1,94 @@
 export const exp = [
   {
-    period: '2019 - present',
-    position: 'Front End Developer',
-    company : 'WellKnown Agency',
-    location: 'Boston, MA',
+    period: "Sept 2018 - Oct 2020",
+    position: "Front End Developer",
+    company: "WellKnown Agency",
+    location: "Boston, MA",
     actions: [
       "Developed multiple single page applications.",
-      "Investigated, fixed, and debugged software defects and bugs.",
+      "Investigated, fixed, and debugged software defects and bugs."
     ]
   },
   {
-    period: '2017 - 2018',
-    position: 'Full Stack Developer',
-    company : 'Launch Academy',
-    location: 'Boston, MA',
+    period: "Oct 2017 - Feb 2018",
+    position: "Full Stack Developer",
+    company: "Launch Academy",
+    location: "Boston, MA",
     actions: [
-      "Completed a 12 week 500+ hours course where I developed, tested, and deployed multiple full stack web applications using ReactJS and Rails.",
+      "Completed a 12 week 500+ hours course where I developed, tested, and deployed multiple full stack web applications using ReactJS and Rails."
     ]
   },
   {
-    period: 'May 2015 - Nov 2015',
-    position: 'Front End Developer Intern.',
-    company : '999.md',
-    location: 'Chisinau, Moldova',
+    period: "May 2015 - Sep 2015",
+    position: "Front End Developer Intern.",
+    company: "999.md",
+    location: "Chisinau, Moldova",
     actions: [
       "Worked on developing, maintaining and supporting web site category – car.",
-      "Planned and developed fully functional prototypes using HTML5, CSS3, JavaScript/jQuery.",
+      "Planned and developed fully functional prototypes using HTML5, CSS3, JavaScript/jQuery."
     ]
-  },
+  }
 ];
 
 export const edu = [
   {
-    period: '2018',
-    institute: 'Microsoft',
-    degree : 'Introduction to ReactJS Certification',
-    location: 'edx.org',
+    period: "2018",
+    institute: "Microsoft",
+    degree: "Introduction to ReactJS Certification",
+    location: "edx.org",
     actions: [
       "JSX and React Components",
       "State, Life Cycle, and Event Handlers",
-      "Lists and Forms",
+      "Lists and Forms"
     ]
   },
   {
-    period: '2018',
-    institute: 'Launch Academy',
-    degree : 'Full Stack Web Developer Certification',
-    location: 'Boston, MA',
+    period: "2018",
+    institute: "Launch Academy",
+    degree: "Full Stack Web Developer Certification",
+    location: "Boston, MA",
     actions: [
       "Rails 5",
       "ReactJS",
       "Object-Oriented Programming and Design",
       "MVC design pattern",
-      "Pair programming",
+      "Pair programming"
     ]
   },
   {
-    period: '2015',
-    institute: 'State University of Moldova',
-    degree : 'AS in Computer Science',
-    location: 'Chisinau, Moldova',
+    period: "2015",
+    institute: "State University of Moldova",
+    degree: "AS in Computer Science",
+    location: "Chisinau, Moldova",
     actions: [
       "Introduction To Computer Science",
       "Algorithms and Data Structures",
       "HTML/CSS/JS",
-      "Calculus II",
-
+      "Calculus II"
     ]
-  },
+  }
 ];
 
 export const skills = [
   {
-    skill: 'JavaScript (React.js)',
+    skill: "JavaScript (React.js)"
   },
   {
-    skill: 'Ruby on Rails 5 ',
+    skill: "Ruby on Rails 5 "
   },
   {
-    skill: 'HTML, CSS, Bootstrap',
+    skill: "HTML, CSS, Bootstrap"
   },
   {
-    skill: 'PostgreSQL',
+    skill: "PostgreSQL"
   },
   {
-    skill: 'Unit Testing',
+    skill: "Unit Testing"
   },
   {
-    skill: 'OOP, MVC ',
+    skill: "OOP, MVC "
   },
   {
-    skill: 'Git',
-  },
+    skill: "Git"
+  }
 ];
